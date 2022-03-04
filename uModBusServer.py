454f3554cf855574782bd05d435af2969817b667
Klint Youngmeyer <kyoungmeyer@emacinc.com>
2022-03-04 11:29:02 -0600

    Add all handle functions to the parent SequentialServer, as the PDU is the same across RTU and TCP

--- uModBusServer.py
+++ uModBusServer.py
@@ -149,6 +149,12 @@ class uModBusServer:
     def setValues(cls, fx, address, values):
         raise NotImplementedException("set context values")
 
+    def _send_data(self, data):
+        raise NotImplementedException("Send data")
+
+    def _send_error_response(self, fx, exception):
+        raise NotImplementedException("Send error response")
+
 
 class uModBusSequentialServer(uModBusServer):
     def __init__(self, server_id, **kwargs):
@@ -173,4 +179,88 @@ class uModBusSequentialServer(uModBusServer):
         _logger.debug("setValues[%d] %d:%d" % (fx, address, len(values)))
         self.databank[self._decode(fx)].setValues(address, values)
 
+    def handleRead(self, fx, buffer):
+        if fx in (Const.READ_HOLDING_REGISTERS, Const.READ_INPUT_REGISTER):
+            _logger.debug("Read {} Register".format(fx))
+            address, count = struct.unpack('>HH', buffer[2:6])
+            if self.validate(fx, address, count):
+                response = struct.pack('>BBB', self.server_id, fx, count*2)
+                rsp_values = self.getValues(fx, address, count)
+                _logger.debug(rsp_values)
+                response += struct.pack('>{}H'.format(count), *list(rsp_values))
+                self._send_data(response)
+            else:
+                self._send_error_response(fx, Const.ILLEGAL_DATA_ADDRESS)
+        else:
+            _logger.debug("Read {} Register".format(fx))
+            address, count = struct.unpack('>HH', buffer[2:6])
+            if self.validate(fx, address, count):
+                response = struct.pack('>BBB', self.server_id, fx, (count + 7)//8)
+                values = self.getValues(fx, address, count)
+                payload = 0
+                for digit in reversed(values):
+                    payload = (payload << 1) | digit
+                payload = payload.to_bytes((count+7)//8, 'little')
+                # payload = int("".join(str(x) for x in values), 2).to_bytes((count+7)//8, 'big')
+                response += payload
+                self._send_data(response)
+            else:
+                self._send_error_response(fx, Const.ILLEGAL_DATA_ADDRESS)
 
+    def handleWriteSingle(self, fx, buffer):
+        _logger.debug("Write Single Coil or Register")
+        address, value = struct.unpack('>HH', buffer[2:6])
+        if self.validate(fx, address, 1):
+            if fx == Const.WRITE_SINGLE_COIL:
+                if value in [0x00, 0xFF00]:
+                    self.setValues(fx, address, ([False] if value == 0x0000 else [True]))
+                else:
+                    self._send_error_response(fx, Const.ILLEGAL_DATA_VALUE)
+            else:
+                self.setValues(fx, address, [value])
+            self._send_data(buffer)
+        else:
+            self._send_error_response(fx, Const.ILLEGAL_DATA_ADDRESS)
+
+    def handleWriteMultiple(self, fx, buffer):
+        if fx == Const.WRITE_MULTIPLE_COILS:
+            address, outputs, count = struct.unpack('>HHB', buffer[2:7])
+            _logger.debug("Write Multiple ({}) Coils".format(outputs))
+            values = struct.unpack('>{}B'.format(count), buffer[7:])
+            _logger.debug("Values to set {}".format(values))
+            if self.validate(fx, address, outputs):
+                val_list = self._bits_to_bool_list(values, outputs)
+                _logger.debug("Bool values to set {}".format(val_list))
+                if val_list is not None:
+                    self.setValues(fx, address, val_list)
+                    response = struct.pack('>BBHH', self.server_id, fx, address, outputs)
+                    self._send_data(response)
+                else:
+                    self._send_error_response(fx, Const.ILLEGAL_DATA_VALUE)
+            else:
+                self._send_error_response(fx, Const.ILLEGAL_DATA_ADDRESS)
+        else:  # Const.WRITE_MULTIPLE_REGISTERS
+            address, num_regs, _count = struct.unpack('>HHB', buffer[2:7])
+            _logger.debug("Write Multiple ({}) Registers".format(num_regs))
+            values = struct.unpack('>{}H'.format(num_regs), buffer[7:])
+            if self.validate(fx, address, num_regs):
+                self.setValues(fx, address, list(values))
+                response = struct.pack('>BBHH', self.server_id, fx, address, num_regs)
+                self._send_data(response)
+            else:
+                self._send_error_response(fx, Const.ILLEGAL_DATA_ADDRESS)
+
+    def handleWrite(self, fx, buffer):
+        if fx in (Const.WRITE_SINGLE_COIL, Const.WRITE_SINGLE_REGISTER):
+            self.handleWriteSingle(fx, buffer)
+        else:
+            self.handleWriteMultiple(fx, buffer)
+
+    def handleRequest(self, fx, buffer):
+        if fx in (Const.READ_COILS, Const.READ_DISCRETE_INPUTS, Const.READ_HOLDING_REGISTERS, Const.READ_INPUT_REGISTER):
+            self.handleRead(fx, buffer)
+        elif fx in (Const.WRITE_SINGLE_COIL, Const.WRITE_SINGLE_REGISTER, Const.WRITE_MULTIPLE_COILS, Const.WRITE_MULTIPLE_REGISTERS):
+            self.handleWrite(fx, buffer)
+        else:
+            # Error Not Supported
+            self._send_error_response(fx, Const.ILLEGAL_FUNCTION)
